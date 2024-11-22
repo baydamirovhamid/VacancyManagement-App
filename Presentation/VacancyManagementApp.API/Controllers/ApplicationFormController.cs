1d0ddc7af48a872d9fb1ced49e32aba0053b22a4
rustem cebrayilov <rustamrj@code.edu.az>
2024-11-22 15:13:58 -0800

    ApplicationForm out GitIgnore

---
+++ Presentation/VacancyManagementApp.API/Controllers/ApplicationFormController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VacancyManagementApp.Application.Features.Commands.ApplicationForm.CreateApplicationForm;
+
+namespace VacancyManagementApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApplicationFormController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ApplicationFormController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateApplicationForm([FromBody] CreateApplicationFormCommandRequest request)
+        {
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
